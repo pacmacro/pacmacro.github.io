a5d03795cd156196eb3738a2a474e2a986a0983c
jleung51 <jleung513@gmail.com>
2018-08-27 02:02:58 -0700

    Reconfigure SFU webspace to use live/index.html

--- scripts/deploy_sfu-webspace.sh
+++ scripts/deploy_sfu-webspace.sh
@@ -8,7 +8,8 @@
 cd `dirname $0`
 cd ../
 
-scp index.html sfu:pub_html/pac-macro.html
-scp -r assets/ sfu:pub_html/
+ssh sfu "mkdir -p pub_html/pac-macro"
+scp index.html sfu:pub_html/pac-macro/index.html
+scp -r assets/ sfu:pub_html/pac-macro/
 
-ssh sfu "chmod 644 pub_html/*.* pub_html/assets/**/*.*"
+ssh sfu "chmod 644 pub_html/pac-macro/*.* pub_html/pac-macro/assets/**/*.*"
