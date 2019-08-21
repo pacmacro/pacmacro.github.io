c07c0845b6c058daf4aa44db1c0b786c224a58f3
Jeffrey Leung <jleung513@gmail.com>
2019-08-21 01:31:44 -0700

    Fix hanging deployment script

--- scripts/deploy_sfu-webspace.sh
+++ scripts/deploy_sfu-webspace.sh
@@ -17,17 +17,17 @@ cd `dirname $0`
 cd ../
 
 echo "Removing previous project files..."
-ssh sfu "chmod 777 ${PROJECT_DIR} && rm -rf ${PROJECT_DIR}"
+ssh -f sfu "chmod 777 ${PROJECT_DIR} && rm -rf ${PROJECT_DIR}"
 
 echo "Creating directory '${PROJECT_DIR}' for the project files..."
-ssh sfu "mkdir -p ${PROJECT_DIR}"
+ssh -f sfu "mkdir -p ${PROJECT_DIR}"
 
 echo "Sending project files to the webspace..."
 scp index.html sfu:${PROJECT_DIR}/index.html
 scp -r assets sfu:${PROJECT_DIR}/
 
 echo "Modifying project file permissions..."
-ssh sfu "chmod 755 ${PACMACRO_DIR} ${PROJECT_DIR}"
-ssh sfu "chmod 644 ${PROJECT_DIR}/*.* ${PROJECT_DIR}/assets/**/*.*"
+ssh -f sfu "chmod 755 ${PACMACRO_DIR} ${PROJECT_DIR}"
+ssh -f sfu "chmod 644 ${PROJECT_DIR}/*.* ${PROJECT_DIR}/assets/**/*.*"
 
 echo "The program has been deployed to http://sfu.ca/~{STUDENT_ID}/pac-macro/${1}."
